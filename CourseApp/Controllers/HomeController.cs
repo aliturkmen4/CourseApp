13274f3e66eb92502525df934e8331a7d8a6ec73
Ali <alym11731996@gmail.com>
2022-02-08 18:36:46 +0300

    Homecontroller was added

---
+++ CourseApp/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseApp.Controllers
+{
+    public class HomeController : Controller //controller sınıfı 1.satırdaki namesapace içinde olduğundan buraya tanıtmak için 1.satırı ekliyoruz!
+    {
+        //yetenek kazandıracak methodlar(action method)
+        public string Index()
+        {
+            //localhost:44380/home/index
+            return "Home/Index";
+        }
+        public string List()
+        {
+            //localhost:44380/home/list
+            return "Home/List";
+        }
+    }
+}
