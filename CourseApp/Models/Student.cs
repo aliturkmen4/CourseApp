c06bbda1c64ced882198844420f8b78863a4d516
Ali <alym11731996@gmail.com>
2022-02-10 05:45:32 +0300

    Tag helpers were studied

---
+++ CourseApp/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseApp.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public bool? Confirm { get; set; } //soru işareti koyarak nullable yaptım!
+    }
+}
